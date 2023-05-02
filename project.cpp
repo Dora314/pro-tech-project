99832c8ab6aab7c575a41fde9b65fea8c7ed4d45
Dora314 <thienduy1001@gmail.com>
2023-05-02 12:11:53 +0700

    test

---
+++ project.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct date
+{
+    int d;
+    int m;
+    int y;
+};
+struct bookInfo
+{
+    char bookName[50];
+    char bookCate[50];
+    char bookAuthor[50];
+    char publisher[50];
+    date year;
+};
+struct bookBorRe
+{
+    bookInfo name;
+    char borName[50];
+    date borDay;
+    date reDay;
+};
+int main()
+{
+}
